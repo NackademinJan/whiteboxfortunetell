dd4b27528fb5f7a2f14f413089ea43f3b4bdff17
Jan Holmenmark <Jan.Holmenmark@yh.nackademin.se>
2017-03-29 18:16:43 +0200

    har skrivit integrationstest med 100% beslutstäckning för FortuneTeller, MagicNumbers och Translator via FortuneTellerIT.java

--- fortune-teller/src/test/java/se/nackademin/FortuneTellerIT.java
+++ fortune-teller/src/test/java/se/nackademin/FortuneTellerIT.java
@@ -1,20 +1,74 @@
 package se.nackademin;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class FortuneTellerIT {
 
     @Test
     public void testGetFortune() {
         FortuneTeller fortuneTeller = new FortuneTeller(new MagicNumbers(), new Translator());
-        fortuneTeller.setAge("16");
+        fortuneTeller.setAge("18");
         fortuneTeller.setHeight("165");
         fortuneTeller.setIncome("11000");
         fortuneTeller.setLocation("Hagfors");
         fortuneTeller.setName("Sture Hagfors");
         String output = fortuneTeller.calculate();
-        assertEquals("Fortune should be calculated correctly", "Din framtid är mjuk. Du borde sluta se. Vi ser att du snart kommer att skaffa en fotboja. Snart kommer du vilja mäta, men då är det viktigt att du är mörk.", output);
+        assertEquals("Fortune should be calculated correctly", "Din framtid är snabb. Du borde sluta se. Vi ser att du snart kommer att skaffa en hund. Snart kommer du vilja resa, men då är det viktigt att du är hård.", output);
+    }
+    
+    
+    @Test
+    public void testEmptyName() {
+        FortuneTeller fortuneTeller = new FortuneTeller(new MagicNumbers(), new Translator());
+        boolean result = fortuneTeller.setName("");
+        assertFalse("should return false for empty name input", result);
+    }
+    @Test
+     public void testNullName() {
+        FortuneTeller fortuneTeller = new FortuneTeller(new MagicNumbers(), new Translator());
+        boolean result = fortuneTeller.setName(null);
+        assertFalse("should return false for null name input", result);
+    }
+    
+    
+    @Test
+    public void testTooLargeIncome() {
+        FortuneTeller fortuneTeller = new FortuneTeller(new MagicNumbers(), new Translator());
+        boolean result = fortuneTeller.setIncome("1000000001");
+        assertFalse("should return false for an excessively large income input", result);
+    }
+    
+    
+    @Test
+    public void testEmptyLocation() {
+        FortuneTeller fortuneTeller = new FortuneTeller(new MagicNumbers(), new Translator());
+        boolean result = fortuneTeller.setLocation("");
+        assertFalse("should return false for empty Location input", result);
+    }
+    @Test
+     public void testNullLocation() {
+        FortuneTeller fortuneTeller = new FortuneTeller(new MagicNumbers(), new Translator());
+        boolean result = fortuneTeller.setLocation(null);
+        assertFalse("should return false for null Location input", result);
+    }
+    
+    
+    @Test
+    public void testInvalidAge() {
+        FortuneTeller fortuneTeller = new FortuneTeller(new MagicNumbers(), new Translator());
+        boolean result = fortuneTeller.setAge("Bingo");
+        assertFalse("should return false for invalid name input", result);
+    }
+    @Test
+     public void testInvalidHeight() {
+        FortuneTeller fortuneTeller = new FortuneTeller(new MagicNumbers(), new Translator());
+        boolean result = fortuneTeller.setHeight("Lång");
+        assertFalse("should return false for invalid height input", result);
     }
 }
