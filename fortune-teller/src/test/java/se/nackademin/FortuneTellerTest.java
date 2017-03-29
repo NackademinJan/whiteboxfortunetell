82e43224bf3c9e93870ad02c065cffc775b119f4
Jan Holmenmark <Jan.Holmenmark@yh.nackademin.se>
2017-03-29 14:57:12 +0200

    har skrivit test med 100% beslutstäckning för FortunetellerTest.java och skapat tom TranslatorTest.java

--- fortune-teller/src/test/java/se/nackademin/FortuneTellerTest.java
+++ fortune-teller/src/test/java/se/nackademin/FortuneTellerTest.java
@@ -23,7 +23,104 @@ public class FortuneTellerTest {
         FortuneTeller fortuneTeller = new FortuneTeller(magicNumbersMock, translatorMock);
         assertEquals("Din framtid är liten. Du borde sluta springa. Vi ser att du snart kommer att skaffa en lönehöjning. Snart kommer du vilja springa, men då är det viktigt att du är liten.", fortuneTeller.calculate());
     }
+    
+    
+    @Test
+    public void testSetNameWithValidInput() {
+        MagicNumbers magicNumbersMock = mock(MagicNumbers.class);
+        Translator translatorMock = mock(Translator.class);
+        FortuneTeller fortuneTeller = new FortuneTeller(magicNumbersMock, translatorMock);
+        boolean result = fortuneTeller.setName("Björn Borg");
+        assertTrue("should return true for valid input", result);
+        verify(magicNumbersMock, times(1)).setName("Björn Borg");
+    }
 
+    @Test
+    public void testSetNameWithNullInput() {
+        MagicNumbers magicNumbersMock = mock(MagicNumbers.class);
+        Translator translatorMock = mock(Translator.class);
+        FortuneTeller fortuneTeller = new FortuneTeller(magicNumbersMock, translatorMock);
+        boolean result = fortuneTeller.setName(null);
+        assertFalse("should return false for null input", result);
+    }
+
+    @Test
+    public void testSetNameWithEmptyStringInput() {
+        MagicNumbers magicNumbersMock = mock(MagicNumbers.class);
+        Translator translatorMock = mock(Translator.class);
+        FortuneTeller fortuneTeller = new FortuneTeller(magicNumbersMock, translatorMock);
+        boolean result = fortuneTeller.setName("");
+        assertFalse("should return false for empty input", result);
+    }
+    
+        
+    @Test
+    public void testSetIncome() {
+        MagicNumbers magicNumbersMock = mock(MagicNumbers.class);
+        Translator translatorMock = mock(Translator.class);
+        FortuneTeller fortuneTeller = new FortuneTeller(magicNumbersMock, translatorMock);
+        boolean result = fortuneTeller.setIncome("25000");
+        assertTrue("should return true for valid input", result);
+        verify(magicNumbersMock, times(1)).setIncome(25000);
+    }
+
+    @Test
+    public void testSetIncomeWithInvalidInput() {
+        MagicNumbers magicNumbersMock = mock(MagicNumbers.class);
+        Translator translatorMock = mock(Translator.class);
+        FortuneTeller fortuneTeller = new FortuneTeller(magicNumbersMock, translatorMock);
+        boolean result = fortuneTeller.setIncome("not an integer");
+        assertFalse("should return false for invalid input", result);
+    }
+    
+    @Test
+    public void testSetIncomeWithVeryLargeInput() {
+        MagicNumbers magicNumbersMock = mock(MagicNumbers.class);
+        Translator translatorMock = mock(Translator.class);
+        FortuneTeller fortuneTeller = new FortuneTeller(magicNumbersMock, translatorMock);
+        boolean result = fortuneTeller.setIncome("10000001");
+        assertFalse("should return false for invalid input", result);
+    }
+    /*
+        @Test
+    public void testSetAIncomeWithNegativeInput() {
+        MagicNumbers magicNumbersMock = mock(MagicNumbers.class);
+        Translator translatorMock = mock(Translator.class);
+        FortuneTeller fortuneTeller = new FortuneTeller(magicNumbersMock, translatorMock);
+        boolean result = fortuneTeller.setIncome("-1");
+        assertFalse("should return false for invalid input", result);
+    }
+    */
+    
+    @Test
+    public void testSetLocationWithValidInput() {
+        MagicNumbers magicNumbersMock = mock(MagicNumbers.class);
+        Translator translatorMock = mock(Translator.class);
+        FortuneTeller fortuneTeller = new FortuneTeller(magicNumbersMock, translatorMock);
+        boolean result = fortuneTeller.setLocation("Karlskrona");
+        assertTrue("should return true for valid input", result);
+        verify(magicNumbersMock, times(1)).setLocation("Karlskrona");
+    }
+
+    @Test
+    public void testSetLocationWithNullInput() {
+        MagicNumbers magicNumbersMock = mock(MagicNumbers.class);
+        Translator translatorMock = mock(Translator.class);
+        FortuneTeller fortuneTeller = new FortuneTeller(magicNumbersMock, translatorMock);
+        boolean result = fortuneTeller.setLocation(null);
+        assertFalse("should return false for null input", result);
+    }
+
+    @Test
+    public void testSetLocationWithEmptyStringInput() {
+        MagicNumbers magicNumbersMock = mock(MagicNumbers.class);
+        Translator translatorMock = mock(Translator.class);
+        FortuneTeller fortuneTeller = new FortuneTeller(magicNumbersMock, translatorMock);
+        boolean result = fortuneTeller.setLocation("");
+        assertFalse("should return false for empty input", result);
+    }
+    
+    
     @Test
     public void testSetAge() {
         MagicNumbers magicNumbersMock = mock(MagicNumbers.class);
@@ -42,4 +139,53 @@ public class FortuneTellerTest {
         boolean result = fortuneTeller.setAge("not an integer");
         assertFalse("should return false for invalid input", result);
     }
+    /*
+    @Test
+    public void testSetAgeWithVeryOldInput() {
+        MagicNumbers magicNumbersMock = mock(MagicNumbers.class);
+        Translator translatorMock = mock(Translator.class);
+        FortuneTeller fortuneTeller = new FortuneTeller(magicNumbersMock, translatorMock);
+        boolean result = fortuneTeller.setAge("131");
+        assertFalse("should return false for invalid input", result);
+    }
+    
+        @Test
+    public void testSetAgeWithNegativeInput() {
+        MagicNumbers magicNumbersMock = mock(MagicNumbers.class);
+        Translator translatorMock = mock(Translator.class);
+        FortuneTeller fortuneTeller = new FortuneTeller(magicNumbersMock, translatorMock);
+        boolean result = fortuneTeller.setAge("-1");
+        assertFalse("should return false for invalid input", result);
+    }
+    */
+    
+        
+    @Test
+    public void testSetHeight() {
+        MagicNumbers magicNumbersMock = mock(MagicNumbers.class);
+        Translator translatorMock = mock(Translator.class);
+        FortuneTeller fortuneTeller = new FortuneTeller(magicNumbersMock, translatorMock);
+        boolean result = fortuneTeller.setHeight("180");
+        assertTrue("should return true for valid input", result);
+        verify(magicNumbersMock, times(1)).setHeight(180);
+    }
+
+    @Test
+    public void testSetHeightWithInvalidInput() {
+        MagicNumbers magicNumbersMock = mock(MagicNumbers.class);
+        Translator translatorMock = mock(Translator.class);
+        FortuneTeller fortuneTeller = new FortuneTeller(magicNumbersMock, translatorMock);
+        boolean result = fortuneTeller.setHeight("not an integer");
+        assertFalse("should return false for invalid input", result);
+    }
+    /*
+    @Test
+    public void testSetHeightWithNegativeInput() {
+        MagicNumbers magicNumbersMock = mock(MagicNumbers.class);
+        Translator translatorMock = mock(Translator.class);
+        FortuneTeller fortuneTeller = new FortuneTeller(magicNumbersMock, translatorMock);
+        boolean result = fortuneTeller.setHeight("-1");
+        assertFalse("should return false for invalid input", result);
+    }
+    */
 }
---
+++ fortune-teller/src/test/java/se/nackademin/TranslatorTest.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.nackademin;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author testautomatisering
+ */
+public class TranslatorTest {
+    
+   /**
+     * Test of getNoun method, of class Translator.
+     */
+    /*
+    @Test
+    public void testGetNoun() {
+        System.out.println("getNoun");
+        int idx = 0;
+        Translator instance = new Translator();
+        String expResult = "";
+        String result = instance.getNoun(idx);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getVerb method, of class Translator.
+     */
+    /*
+    @Test
+    public void testGetVerb() {
+        System.out.println("getVerb");
+        int idx = 0;
+        Translator instance = new Translator();
+        String expResult = "";
+        String result = instance.getVerb(idx);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getAdjective method, of class Translator.
+     */
+    /*
+    @Test
+    public void testGetAdjective() {
+        System.out.println("getAdjective");
+        int idx = 0;
+        Translator instance = new Translator();
+        String expResult = "";
+        String result = instance.getAdjective(idx);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    */
+}
