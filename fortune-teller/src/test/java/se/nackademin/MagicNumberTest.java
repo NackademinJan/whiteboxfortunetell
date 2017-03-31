8624a5eab47720f56ad87de29f5f7df2b6554b1b
Jan Holmenmark <Jan.Holmenmark@yh.nackademin.se>
2017-03-31 15:16:12 +0200

    lade till fler tesfall för att testa lite algoritmerna för A-E med olika extremvärden i MagicNumberTest.java

--- fortune-teller/src/test/java/se/nackademin/MagicNumberTest.java
+++ fortune-teller/src/test/java/se/nackademin/MagicNumberTest.java
@@ -109,9 +109,9 @@ public class MagicNumberTest {
     
     
     /*
-     * testCalculateAIs Breakable tries to break calculateA by causing that method to return a result that is not a result between 0-9, the current version produces a result of 10
      *
-     *
+     * test tries to break calculateA by causing that method to return a result that is not a result between 0-9, the current version produces a result of 10
+     * 
      */
     
     @Test
@@ -123,6 +123,208 @@ public class MagicNumberTest {
         assertTrue("calculateA should always produce a result between 0-9", result);    
     }
     
+    /*
+     * test tries to break calculateA by providing minimal positive input values (that arent empty strings or null values)
+     */
+    
+    @Test
+    public void testCalculateAMinimalInput() {
+        magicNumbers.setName("J");
+        magicNumbers.setAge(0);
+        int resultNr = magicNumbers.calculateA();
+        boolean result = !(resultNr < 0) && !(resultNr >9);
+        assertTrue("calculateA should always produce a result between 0-9", result);    
+    }
+    
+    /*
+     * test tries to break calculateA by providing negative input values (where possible)
+     */
+    
+    @Test
+    public void testCalculateANegativeInput() {
+        magicNumbers.setName("J");
+        magicNumbers.setAge(-1);
+        int resultNr = magicNumbers.calculateA();
+        boolean result = !(resultNr < 0) && !(resultNr >9);
+        assertTrue("calculateA should always produce a result between 0-9", result);    
+    }
+    
+    /*
+     * test tries to break calculateA by providing large positive input values (that do not violate constraints of the datatype or established rules like max 10 000 000 for income)
+     */
+    
+    @Test
+    public void testCalculateALargerInputs() {
+        magicNumbers.setName("a b c d e f g h i j k l m n o p q r s t u v x y z");
+        magicNumbers.setAge(10000001);
+        int resultNr = magicNumbers.calculateA();
+        boolean result = !(resultNr < 0) && !(resultNr >9);
+        assertTrue("calculateA should always produce a result between 0-9", result);    
+    }
+    
+    
+   /*
+    * test tries to break calculateB by providing minimal positive input values (that arent empty strings or null values)
+    */
+    @Test
+    public void testCalculateBMinimalInput() {
+        magicNumbers.setLocation("x");
+        magicNumbers.setIncome(0);
+        int resultNr = magicNumbers.calculateB();
+        boolean result = !(resultNr < 0) && !(resultNr >9);
+        assertTrue("calculateA should always produce a result between 0-9", result);    
+    }
+    
+    /*
+     * test tries to break calculateB by providing negative input values (where possible)
+     */
+    
+    @Test
+    public void testCalculateBNegativeInput() {
+        magicNumbers.setLocation("x");
+        magicNumbers.setIncome(-1);
+        int resultNr = magicNumbers.calculateB();
+        boolean result = !(resultNr < 0) && !(resultNr >9);
+        assertTrue("calculateA should always produce a result between 0-9", result);    
+    }
+    
+    /*
+     * test tries to break calculateB by providing large positive input values (that do not violate constraints of the datatype or established rules like max 10 000 000 for income)
+     */
+    
+    @Test
+    public void testCalculateBLargerInputs() {
+        magicNumbers.setLocation("a b c d e f g h i j k l m n o p q r s t u v x y z");
+        magicNumbers.setIncome(10000000);
+        int resultNr = magicNumbers.calculateB();
+        boolean result = !(resultNr < 0) && !(resultNr >9);
+        assertTrue("calculateA should always produce a result between 0-9", result);    
+    }
+    
+   /*
+    * test tries to break calculateC by providing minimal positive input values (that arent empty strings or null values)
+    */
+    @Test
+    public void testCalculateCMinimalInput() {
+        magicNumbers.setLocation("x");
+        magicNumbers.setIncome(0);
+        magicNumbers.setHeight(0);
+        int resultNr = magicNumbers.calculateC();
+        boolean result = !(resultNr < 0) && !(resultNr >9);
+        assertTrue("calculateA should always produce a result between 0-9", result);    
+    }
+    
+    /*
+     * test tries to break calculateC by providing negative input values (where possible)
+     */
+    
+    @Test
+    public void testCalculateCNegativeInput() {
+        magicNumbers.setLocation("x");
+        magicNumbers.setIncome(-1);
+        magicNumbers.setHeight(-1);
+        int resultNr = magicNumbers.calculateC();
+        boolean result = !(resultNr < 0) && !(resultNr >9);
+        assertTrue("calculateA should always produce a result between 0-9", result);    
+    }
+    
+    /*
+     * test tries to break calculateC by providing large positive input values (that do not violate constraints of the datatype or established rules like max 10 000 000 for income)
+     */
+    
+    @Test
+    public void testCalculateCLargerInputs() {
+        magicNumbers.setLocation("a b c d e f g h i j k l m n o p q r s t u v x y z");
+        magicNumbers.setIncome(10000000);
+        magicNumbers.setHeight(10000001);
+        int resultNr = magicNumbers.calculateC();
+        boolean result = !(resultNr < 0) && !(resultNr >9);
+        assertTrue("calculateA should always produce a result between 0-9", result);    
+    }
+    
+    
+   /*
+    * test tries to break calculateD by providing minimal positive input values (that arent empty strings or null values)
+    */
+    @Test
+    public void testCalculateDMinimalInput() {
+        magicNumbers.setLocation("x");
+        magicNumbers.setIncome(0);
+        magicNumbers.setHeight(0);
+        int resultNr = magicNumbers.calculateD();
+        boolean result = !(resultNr < 0) && !(resultNr >9);
+        assertTrue("calculateA should always produce a result between 0-9", result);    
+    }
+    
+    /*
+     * test tries to break calculateD by providing negative input values (where possible)
+     */
+    
+    @Test
+    public void testCalculateDNegativeInput() {
+        magicNumbers.setLocation("x");
+        magicNumbers.setIncome(-1);
+        magicNumbers.setHeight(-1);
+        int resultNr = magicNumbers.calculateD();
+        boolean result = !(resultNr < 0) && !(resultNr >9);
+        assertTrue("calculateA should always produce a result between 0-9", result);    
+    }
+    
+    /*
+     * test tries to break calculateD by providing large positive input values (that do not violate constraints of the datatype or established rules like max 10 000 000 for income)
+     */
+    
+    @Test
+    public void testCalculateDLargerInputs() {
+        magicNumbers.setLocation("a b c d e f g h i j k l m n o p q r s t u v x y z");
+        magicNumbers.setIncome(10000000);
+        magicNumbers.setHeight(10000001);
+        int resultNr = magicNumbers.calculateD();
+        boolean result = !(resultNr < 0) && !(resultNr >9);
+        assertTrue("calculateA should always produce a result between 0-9", result);    
+    }
+    
+    
+   /*
+    * test tries to break calculateE by providing minimal positive input values (that arent empty strings or null values)
+    */
+    @Test
+    public void testCalculateEMinimalInput() {
+        magicNumbers.setAge(0);
+        magicNumbers.setIncome(0);
+        magicNumbers.setHeight(0);
+        int resultNr = magicNumbers.calculateE();
+        boolean result = !(resultNr < 0) && !(resultNr >9);
+        assertTrue("calculateA should always produce a result between 0-9", result);    
+    }
+    
+    /*
+     * test tries to break calculateE by providing negative input values (where possible)
+     */
+    
+    @Test
+    public void testCalculateENegativeInput() {
+        magicNumbers.setAge(-1);
+        magicNumbers.setIncome(-1);
+        magicNumbers.setHeight(-1);
+        int resultNr = magicNumbers.calculateE();
+        boolean result = !(resultNr < 0) && !(resultNr >9);
+        assertTrue("calculateA should always produce a result between 0-9", result);    
+    }
+    
+    /*
+     * test tries to break calculateE by providing large positive input values (that do not violate constraints of the datatype or established rules like max 10 000 000 for income)
+     */
+    
+    @Test
+    public void testCalculateELargerInputs() {
+        magicNumbers.setAge(10000001);
+        magicNumbers.setIncome(10000000);
+        magicNumbers.setHeight(10000001);
+        int resultNr = magicNumbers.calculateE();
+        boolean result = !(resultNr < 0) && !(resultNr >9);
+        assertTrue("calculateA should always produce a result between 0-9", result);    
+    }
     
     
 }
